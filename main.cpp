bb4671d34a4f48a6b5359e77eb8d32e83e95ef19
Dmitry Kudrin <qdrin@mail.ru>
2021-11-22 21:59:33 +0300

    1. Grayscaling, 2. Eroding, 3. Trying to findContours

--- main.cpp
+++ main.cpp
@@ -17,10 +17,36 @@ int main()
     cin.get();
     return -1;
   }
-  String wName = "Ерундульки";
+  int th_val = 200, th_max = 255;  // threshhold value and max(black)
+  Mat imgGray, imgTh, imgEr;
+  Mat kernel_3x3;
+  Mat kernel_5x5;
+  cvtColor(image, imgGray, COLOR_BGR2GRAY);
+  adaptiveThreshold(imgGray, imgTh, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 7, 21);
+  erode(imgTh, imgEr, getStructuringElement(MORPH_RECT, Size(3,3)));
+
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+  findContours(imgEr, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+  Mat drawing = Mat::zeros(imgEr.size(), CV_8UC3);
+  for(int i=0; i < contours.size(); i++)
+    drawContours(drawing, contours, i, 255, 2, LINE_8, hierarchy, 0);
+  String wName;
+  wName = "Ерундульки";
   namedWindow(wName);
   imshow(wName, image);
+  wName = "Ерундульки threshold";
+  namedWindow(wName);
+  imshow(wName, imgTh);
+  wName = "Ерундульки erode";
+  namedWindow(wName);
+  imshow(wName, imgEr);
+  wName = "Ерундульки contours";
+  namedWindow(wName);
+  imshow(wName, drawing);
   waitKey(0);
-  destroyWindow(wName);
+  destroyAllWindows();
+
   return 0;
 }
